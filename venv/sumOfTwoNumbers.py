be87b621f46190b7ec17482d3f9ec78b3f441402
Mandar <edxsmtba@bbaaviation.net>
2021-05-30 23:09:39 +0530

    Initial commit

---
+++ venv/sumOfTwoNumbers.py
@@ -0,0 +1,4 @@
+number1 = input("Input first number :")
+number2 = input("Input second number :")
+sum = float(number1)+float(number2)
+print("Sum of {0} and {1} is {2} " .format(number1,number2,sum))
